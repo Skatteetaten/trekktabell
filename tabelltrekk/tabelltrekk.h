93cce88a88c30e02f9600ccbfca225de4e6aab27
aleksanderaleksic <aleksander@aleksic.no>
2017-03-03 21:23:42 +0100

    Init project

---
+++ tabelltrekk/tabelltrekk.h
@@ -0,0 +1,19 @@
+//
+//  tabelltrekk.h
+//  tabelltrekk
+//
+//  Created by Aleksander Aleksic on 03.03.2017.
+//  Copyright © 2017 Aleksic Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for tabelltrekk.
+FOUNDATION_EXPORT double tabelltrekkVersionNumber;
+
+//! Project version string for tabelltrekk.
+FOUNDATION_EXPORT const unsigned char tabelltrekkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <tabelltrekk/PublicHeader.h>
+
+
