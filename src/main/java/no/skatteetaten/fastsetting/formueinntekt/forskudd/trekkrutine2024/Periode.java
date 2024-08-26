46779bd4e1a3625debd0f64b90ba02a52b8b713d
Per Ristun <per.ristun@skatteetaten.no>
2024-08-26 10:58:46 +0200

    SPAP-25398 Økt maxtrekkgrunnlag for alle perioder

--- src/main/java/no/skatteetaten/fastsetting/formueinntekt/forskudd/trekkrutine2024/Periode.java
+++ src/main/java/no/skatteetaten/fastsetting/formueinntekt/forskudd/trekkrutine2024/Periode.java
@@ -1,13 +1,13 @@
 package no.skatteetaten.fastsetting.formueinntekt.forskudd.trekkrutine2024;
 
 public enum Periode {
-    PERIODE_1_MAANED(12.12d, 10.5d,           12d, 11d,             12d, 10.5d, 100, 99800 ),
-    PERIODE_14_DAGER(26.26d, 23d,             26d, 24d,             26d, 23d,   50, 46000 ),
-    PERIODE_1_UKE   (52.52d, 46d,             52d, 48d,             52d, 46d,   20, 23000 ),
-    PERIODE_4_DAGER (60.60d, (60d*46d)/52d,   91d, (91d*11d)/12d,   91d, (91d*10.5d)/12d, 20, 19900 ),
-    PERIODE_3_DAGER (80.80d, (80d*46d)/52d,  122d, (122d*11d)/12d, 122d, (122d*10.5d)/12d, 20, 14900 ),
-    PERIODE_2_DAGER(121.20d, (120d*46d)/52d, 183d, (183d*11d)/12d, 183d, (183d*10.5d)/12d, 20, 9900 ),
-    PERIODE_1_DAG  (242.40d, (240d*46d)/52d, 365d, (365d*11d)/12d, 365d, (365d*10.5d)/12d, 20, 4900 );
+    PERIODE_1_MAANED(12.12d, 10.5d,           12d, 11d,             12d, 10.5d, 100, 200000 ),
+    PERIODE_14_DAGER(26.26d, 23d,             26d, 24d,             26d, 23d,   50, 100000 ),
+    PERIODE_1_UKE   (52.52d, 46d,             52d, 48d,             52d, 46d,   20, 50000 ),
+    PERIODE_4_DAGER (60.60d, (60d*46d)/52d,   91d, (91d*11d)/12d,   91d, (91d*10.5d)/12d, 20, 40000 ),
+    PERIODE_3_DAGER (80.80d, (80d*46d)/52d,  122d, (122d*11d)/12d, 122d, (122d*10.5d)/12d, 20, 30000 ),
+    PERIODE_2_DAGER(121.20d, (120d*46d)/52d, 183d, (183d*11d)/12d, 183d, (183d*10.5d)/12d, 20, 20000 ),
+    PERIODE_1_DAG  (242.40d, (240d*46d)/52d, 365d, (365d*11d)/12d, 365d, (365d*10.5d)/12d, 20, 10000 );
 
     final double inntektsPeriode;
     final double trekkPeriode;
